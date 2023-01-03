5a48994acbaa89d267772c5f8590cca75530ec0a
loisRK <loiskim150@gmail.com>
2023-01-03 11:04:37 +0900

    kakao redirect uri 주소 https://www.dulgi.net으로 변경

--- src/components/KakaoLoginData.js
+++ src/components/KakaoLoginData.js
@@ -1,5 +1,6 @@
 export const REST_API_KEY = "3bd21a86de934233cc9f3012709c304f";
-export const REDIRECT_URI = "http://175.41.229.233:3000/checkMember";
+// export const REDIRECT_URI = "http://175.41.229.233:3000/checkMember";
+export const REDIRECT_URI = "https://www.dulgi.net/checkMember";
 const code = new URL(window.location.href).searchParams.get("code");
 
 // 카카오 인증 받기
