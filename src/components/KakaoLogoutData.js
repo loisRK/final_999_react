dae63f7b0c7e4b6d82c6b1769a5797e5626839a6
loisRK <loiskim150@gmail.com>
2023-01-05 12:50:11 +0900

    index logo file 수정

--- src/components/KakaoLogoutData.js
+++ src/components/KakaoLogoutData.js
@@ -2,7 +2,7 @@ export const REST_API_KEY = "3bd21a86de934233cc9f3012709c304f";
 // export const REDIRECT_URI = "http://175.41.229.233:3000/kakaoLogout";
 // export const REDIRECT_URI2 = "http://175.41.229.233:3000/kakaoWithdraw";
 export const REDIRECT_URI = "https://www.dulgi.net/kakaoLogout";
-export const REDIRECT_URI2 = "http://www.dulgi.net/kakaoWithdraw";
+export const REDIRECT_URI2 = "https://www.dulgi.net/kakaoWithdraw";
 
 // 카카오 인증 받기
 export const KAKAO_LOGOUT_URL = `https://kauth.kakao.com/oauth/logout?client_id=${REST_API_KEY}&logout_redirect_uri=${REDIRECT_URI}`;
